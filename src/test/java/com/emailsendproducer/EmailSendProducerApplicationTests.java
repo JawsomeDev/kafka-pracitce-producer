3ac42d9df687d6931a10ec9393a1a1e2e21fdffe
JawsomeDev <hyuk2000s@gmail.com>
2025-11-12 15:25:21 +0900

    first commit

---
+++ src/test/java/com/emailsendproducer/EmailSendProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emailsendproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSendProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
